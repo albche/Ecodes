6aab9f48f711ee708420af150fb431b4078275db
StephenTan12 <sttan@ucsd.edu>
2022-11-03 00:16:53 -0700

    initial refactoring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Item from "./Item.js"
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Item from "./components/Item.js"
 
 function App() {
   return (
     <div>
-        <Routes>
-          <Route path="/item" element={<Item />} />
-        </Routes>
+      <Routes>
+        <Route path="/item" element={<Item />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Item.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+const stats = {
+  efficiency: 100,
+  goodness: 10,
+  coolness: 10,
+  awesomeness: 10,
+};
+
+const Item = (props) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [statsList, setStatsList] = useState([]);
+  const id = searchParams.get("id");
+  const title = "Cool Item!";
+  const impact = "";
+  const recommendations = "";
+
+  useEffect(() => {
+    // fetch from database here
+    let list = Object.entries(stats).map((stat) => (
+      <h4>
+        {stat[0]}: {String(stat[1])}
+      </h4>
+    ));
+
+    setStatsList(list);
+  }, []);
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <div>{statsList}</div>
+    </div>
+  );
+};
+
+export default Item;
